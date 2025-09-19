c72498c6f511bceb2df1561c25c3a94b980c368a
PauloinaVidal22 <paulina.vidal@correo.ucu.edu.uy>
2025-09-19 17:21:28 -0300

    bad identification of CWE-327 & CWE-347 (jsonwebtoken from nodejs implements SHA256 by default. Vulnerability identified CWE-1390 & mitigated

--- services/backend/src/utils/jwt.ts
+++ services/backend/src/utils/jwt.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET || JWT_SECRET.length < 32) {
-  throw new Error('JWT_SECRET is missing or too weak. Use at least 32 random characters.'); // CWE-522
+  throw new Error('JWT_SECRET is missing or too weak.');
 }
 
 
